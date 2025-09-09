73aec914f5a6ca3fde1972142de06994de0aad10
Chacha Chacha <calex2607@gmail.com>
2025-09-10 00:28:59 +0300

    supervisor

--- attendance_system/lib/attendance_system.ex
+++ attendance_system/lib/attendance_system.ex
@@ -1,9 +1,33 @@
-defmodule AttendanceSystem do
-  @moduledoc """
-  AttendanceSystem keeps the contexts that define your domain
-  and business logic.
+# lib/attendance_system/application.ex
+defmodule AttendanceSystem.Application do
+  @moduledoc false
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      AttendanceSystemWeb.Telemetry,
+      # Start the Ecto repository
+      AttendanceSystem.Repo,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: AttendanceSystem.PubSub},
+      # Start Finch
+      {Finch, name: AttendanceSystem.Finch},
+      # Start Oban (only in non-test environments)
+      {Oban, Application.fetch_env!(:attendance_system, Oban)},
+      # Start the Endpoint (http/https)
+      AttendanceSystemWeb.Endpoint
+    ]
+
+    opts = [strategy: :one_for_one, name: AttendanceSystem.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  @impl true
+  def config_change(changed, _new, removed) do
+    AttendanceSystemWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
 end
